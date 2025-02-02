f1dfe8f950239c8f48cabac5f3da6e04b20934cc
ermaozi <admin@ermao.net>
2025-02-02 14:00:19 +0800

    优化url

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,7 +2,22 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/vpnrecs/",
-  "/scamvpn/",
-  "/doc/",
+  {
+    text: "机场推荐",
+    icon: "mdi:airplane-takeoff",
+    prefix: "机场推荐/",
+    link: "vpnrecs/",
+  },
+  {
+    text: "跑路机场",
+    icon: "mdi:airplane-landing",
+    prefix: "跑路机场/",
+    link: "scamvpn/",
+  },
+  {
+    text: "文档",
+    icon: "book",
+    prefix: "文档/",
+    link: "doc/",
+  },
 ]);
